c5506b228359bce88f333b919825082b71b9dd21
unknown <hmphrmsn@gmail.com>
2016-12-01 06:38:41 +0300

    Output as List

--- prime.py
+++ prime.py
@@ -1,21 +1,3 @@
-# def gen_prime(number):
-
-# 	prime_nos = []
-# 	if number < 0:
-# 		return "only positive numbers allowed"
-# 	if type(number) != "int":
-# 		return "Invalid Input!!Only numbers allowed"		
-
-# 	else: 
-# 		for val in range(2,number):
-# 			div = 0
-# 			for num in range(2,val - 1):
-# 				if val % num == 0:
-# 					div = div + 1
-# 			if div == 0:
-# 				print val
-			
-# print gen_prime(-10)
 def gen_prime(number):
 
 	prime_nos = []
