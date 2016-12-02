3ee9f8a614196543ef3b8e8e5e16485034e4280f
unknown <hmphrmsn@gmail.com>
2016-12-02 19:14:23 +0300

    Changes: Accept user input and output on cmd line

--- weather.py
+++ weather.py
@@ -1,20 +1,37 @@
 #simple API using HTTP client library
 #By hms91 
 
-import requests
+import urllib
 import json
-#key from wunderground.com to enable the use of their weather API
-key = "[7c5e41c395b1f11a]"
 
-#get a response from wunderground.com
-data = requests.get('http://api.wunderground.com/api/'+ key +'/geolookup/conditions/q/Kenya/Nairobi.json').json()
+#Keeps our program in loop
+while True:
 
-#set the current city to the location variable
-location = data['location']['city']
+    #get user's input
+    address1 = raw_input('Enter Country: ')
+    address2 = raw_input('Enter City: ')
 
-#set the temperature from our json to temp_c
-temp_c = data['current_observation']['temp_c']
+    #check for empty 
+    if len(address1) == "" : break
+    if len(address2) == "" : break
 
-#print out the results
-print "The current temperature in %s is: %s C" % (location, temp_c)
+    #url to access wunderground.com api
+    #use http library to get response from wunderground.com
+    #7c5e41c395b1f11a key from the site to allow us access
+    url = 'http://api.wunderground.com/api/7c5e41c395b1f11a/geolookup/conditions/q/'+address1+'/'+address2+'.json'
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+
+    #make sure that we received good data
+    try: js = json.loads(str(data))
+    except: js = None
+
+    #assign values from returned json data 
+    weather = js["current_observation"]["weather"]
+    weather1 = js["current_observation"]["relative_humidity"]
+    weather2 = js["current_observation"]["feelslike_string"]
+
+    #tell the user the current weather and what it feels like
+    print "The Current weather in %s %s is: %s %s It will feel like %s" % (address2, address1, weather, weather1, weather2)
 
